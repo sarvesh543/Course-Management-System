3bd1f8a1321ff3b0f55175b29c8c0646237758f4
Sarvesh Jadhav <sarveshjadhav543@gmail.com>
2022-11-17 19:41:51 +0530

    db url changes

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config()
 
 //db connections
-mongoose.connect("mongodb://localhost:27017/ADP", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
